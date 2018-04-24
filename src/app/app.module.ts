6c5eb87f13818304cdeed8ccda6742e47f212ab8
Kelber Stuchi <kelberstuchi@gmail.com>
2018-04-24 15:07:53 -0300

    create components & tested ng lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PeopleComponent } from './people/people.component';
+import { PeopleDetailComponent } from './people/people-detail/people-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    PeopleComponent,
+    PeopleDetailComponent
   ],
   imports: [
     BrowserModule,
